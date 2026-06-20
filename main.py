3cbb5c720c95dfdb1c0c546f48b24a0c8fbde5a0
FaithMabz <faithmabaleng@gmail.com>
2026-06-20 23:16:26 +0200

    Added CV detection

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ with open("sample_cv.txt", "r", encoding="utf-8") as file:
 word_count = len(cv_text.split())
 
 print("\nWord count:", word_count)
+
+print("\nCV Section Check:")
+if "Education" in cv_text:
+    print("Education section found.")
+else:
+    print("Education section missing.")
+if "Skills" in cv_text:
+    print("Skills section found.")
+else:
+    print("Skills section missing.")
+if "Experience" in cv_text:
+    print("Experience section found.")
+else:
+    print("Experience section missing.")
